98fdf6cdaff5182dd6efcbc4fc03645beef2f3a8
Ammarah <anizam1000@gmail.com>
2021-11-30 14:01:15 +0000

    automation tests for login and search

---
+++ cypress/integration/login.spec.js
@@ -0,0 +1,20 @@
+/// <reference types="cypress" />
+import LoginPage from "../../pageobjects/login.po";
+
+const loginob = new LoginPage();
+
+describe('Verify login functionality', () => {
+
+    it('Should successfully login into the app', () => {
+        loginob.navigate();
+        loginob.login();
+        cy.url().should('include', 'https://www.xperthr.co.uk/')
+        cy.contains("My dashboard").should("be.visible")
+
+
+    })
+
+
+
+
+})
---
+++ cypress/integration/search.spec.js
@@ -0,0 +1,34 @@
+/// <reference types="cypress" />
+import LoginPage from "../../pageobjects/login.po";
+import SearchPage from "../../pageobjects/search.po";
+
+const loginob = new LoginPage();
+const searchob = new SearchPage();
+describe('Verify search and filter functionality', () => {
+    beforeEach(function() {
+        loginob.navigate();
+        loginob.login();
+
+    })
+    it('Should Show 20 Search Results Per Page for the keyword Pay', () => {
+        searchob.searchaKeyword();
+        cy.url().should('include', 'keywords=Pay')
+        searchob.searchResults.should("contain.text", "Pay");
+        searchob.searchResults.should("have.length", 20);
+
+    })
+
+
+    it('Should Show Search Results According to the Applied Filter', () => {
+        searchob.filterResults();
+        cy.contains('Law reports').click({ force: true });
+        cy.get('h3').should('contain.text', 'Viewing results by:')
+        cy.contains('Law reports').should('be.visible')
+    })
+
+
+
+
+
+
+})
